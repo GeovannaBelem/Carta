3d4089f4e690eb167b4a58db74fb1784a0d1b59b
Geovanna Belem <102627701+GeovannaBelem@users.noreply.github.com>
2024-11-12 20:14:53 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function AbrirCarta(){
+    document.getElementById('trianguloCarta').style.rotate = '180deg';
+    document.getElementById('carta').style.display = 'block';
+    document.getElementById('carta').style.top = '-46vh';
+    document.getElementById('quadradoCarta').style.top = '22vh';
+    document.getElementById('trianguloCarta').style.top = '-19vh';
+}
